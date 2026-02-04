83ca133d6a20344f3fc23489664c07bae2a3b244
Tomi Abe <abe.tomilayo@yahoo.com>
2026-02-04 12:02:58 +0100

    feat: Add React-based CMS previews with styled components

--- src/cms/previews/HomePreview.tsx
+++ src/cms/previews/HomePreview.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import HomePage from '../../../components/Home/HomePage';
 import PreviewWrapper from './PreviewWrapper';
 
